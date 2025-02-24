f9ad902c8793190339311e9b7de4e4f37b7fead8
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-02-24 17:13:36 +0100

    feat: calculate the ticket price and discounts

--- js/app.js
+++ js/app.js
@@ -4,3 +4,25 @@ console.log(myAge);
 // chiedere quanti chilometri deve fare
 const enterKm = prompt("Enter the km to be done");
 console.log(enterKm);
+// calcolare il prezzo del biglietto in base ai km
+if (myAge>18 && myAge<65){
+    const priceKm = (0.21);
+    const tottPrice = priceKm * enterKm;
+    console.log(tottPrice)
+// altrimenti se l'utente e minorenne applicare uno sconto del 20%
+}else if (myAge<18) {
+    const priceKm = (0.21);
+    const tottPrice = priceKm * enterKm;
+    const twentypercent = 20 
+    const percentageKm = ((twentypercent / 100) * tottPrice)
+    const priceDiscountMinor = tottPrice - percentageKm
+    console.log(priceDiscountMinor)
+// altrimenti se l' utente e un over 65 applicare uno sconto del 40%  
+}else if (myAge>65){
+    const priceKm = (0.21);
+    const tottPrice = priceKm * enterKm;
+    const fortyPercent = 40
+    const fortyPercentKm = ((fortyPercent / 100) * tottPrice)
+    const priceDiscountOver = tottPrice - fortyPercentKm
+    console.log(priceDiscountOver)
+} 
